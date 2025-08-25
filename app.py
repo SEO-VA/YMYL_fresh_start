1b0740fe5480826d3f2aa89d7c0a283d5cc014f1
Victor.A <artufel.v@gmail.com>
2025-08-25 02:47:14 +0200

    ho

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def main():
 💡 **How to: Analyse content from draft document**
 
 1. Create a copy of the draft document
-2. Remove anything that is not part of the content to audit
+2. Remove anything that is not part of the content to audit  
     Note: You can leave images/screenshot in content body
 3. Download the draft document as HTML
 4. Click "Browse files" and upload downloaded document
